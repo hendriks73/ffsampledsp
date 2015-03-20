35a896053105d9a93df588b5471947a374a8720f
Hendrik Schreiber <hs@tagtraum.com>
2015-03-20 09:15:42 +0100

    Fixed little typo.

--- ffsampledsp-java/src/main/java/com/tagtraum/ffsampledsp/package-info.java
+++ ffsampledsp-java/src/main/java/com/tagtraum/ffsampledsp/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Core classes of the <em>FFSampledSP</em> library, is a simple
+ * Core classes of the <em>FFSampledSP</em> library—a simple
  * implementation of the {@link javax.sound.sampled.spi} interfaces based on
  * <a href="http://www.ffmpeg.org" target="_top">FFmpeg</a>.
  */
